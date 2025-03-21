e36eb2955dea2fdb4e028b04f832b25d7f8cdb8c
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-21 21:20:35 +0100

    Create reusable button

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+type ButtonProps = {
+  id: string;
+  title: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  containerClass?: string;
+};
+
+const Button = ({ id, title, leftIcon, containerClass }: ButtonProps) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+    </button>
+  );
+};
+
+export default Button;
